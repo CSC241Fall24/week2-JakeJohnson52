b646b59cf0600fbf1b9cbba0ad57fe3a156886b4
JakeJohnson52 <157393198+JakeJohnson52@users.noreply.github.com>
2024-09-05 12:47:31 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -3,8 +3,8 @@
 public class Q1WeirdFunction {
 
     public static int fRecursive(int n) {
-        // TODO: Implement the recursive function
-        return 0;
+        if(n < 3)return n;
+        else return fRecursive(n-1) + 2* fRecursive(n-2) + 3* fRecursive(n-3); 
     }
 
     public static int fIterative(int n) {
