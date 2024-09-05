9bb8dca8106e8ae3bc2c588f649470ec1a2ecc63
JakeJohnson52 <157393198+JakeJohnson52@users.noreply.github.com>
2024-09-05 12:38:44 -0400

    Update Q4ReverseString.java

--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -4,6 +4,6 @@ public class Q4ReverseString {
 
     public static String reverse(String s) {
         if(s.length() == 0) return "";
-        else return s.substring(s.length() - 1) + reverse(s.substring(0,s.length() -  1);
+        else return s.substring(s.length() - 1) + reverse(s.substring(0,s.length() -  1));
     }
 }
